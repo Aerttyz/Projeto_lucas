da087d7d688ee2f6618863a990b823e5b1d417e4
Thauã Magalhães Paulino Lucas <thaua.l@academico.ifrn.edu.br>
2022-11-29 14:28:21 -0300

    adicionado update.php

---
+++ telas/perfil/update.php
@@ -0,0 +1,73 @@
+<?php
+
+include('conexao.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])){
+
+  $email = $_POST['email'];
+  $nome = $_POST['nome'];
+  $senha = MD5($_POST['senha']);
+  $novasenha = MD5($_POST['novasenha']);
+
+  echo $email;
+  echo $senha;
+
+  if(strlen($email)==0){
+    echo "Preencha o novo e-mail";
+  } else if (strlen($senha)==0){
+    echo "Preencha sua senha";
+  } else {
+    $email = $mysqli->real_escape_string($_POST['email']);
+
+    $sql_code = "SELECT * FROM `users` WHERE email = '$email' and senha = '$senha'";
+    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código: " . $mysqli->error);
+  
+    $quantidade = $sql_query->num_rows;
+
+    if($quantidade == 1){
+      $usuario = $sql_query->fetch_assoc();
+
+      if(!isset($_SESSION)) {
+        session_start();
+      }
+
+      $_SESSION['user'] = $usuario['id'];
+      $_SESSION['name'] = $usuario['nome'];
+
+      echo $usuario['nome'];
+      echo $usuario['email'];
+
+      echo"<script language='javascript' type='text/javascript'>
+        alert('Login realizado com sucesso!');window.location
+        .href='./telas/partitura/partitura.html';</script>";
+      header('location:telas/partitura/partitura.html');
+    } else {
+      echo "Falha ao logar! E-mail ou senha incorretos";
+      echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='index.html';</script>";
+      die();
+    }
+  }
+}
+
+/*$login = $_POST['login'];
+$entrar = $_POST['entrar'];
+$senha = md5($_POST['senha']);
+$connect = mysqli_connect('localhost','root','');
+$db = mysqli_select_db('db');
+  if (isset($entrar)) {
+
+    $verifica = mysqli_query("SELECT * FROM usuarios WHERE login =
+    '$login' AND senha = '$senha'") or die("erro ao selecionar");
+      if (mysqli_num_rows($verifica)<=0){
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='index.html';</script>";
+        die();
+      }else{
+        setcookie("login",$login);
+        header("Location:index.php");
+      }
+  }*/
+?>
