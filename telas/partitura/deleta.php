eb541a240a1cccaad720f5ecb203a4ac2e16c1b6
Thaua <thauanlucascpl@gmail.com>
2022-12-19 11:59:29 -0300

    funcao de excluir partitura

--- telas/partitura/deleta.php
+++ telas/partitura/deleta.php
@@ -2,7 +2,7 @@
 include('../../conexao.php');
 
 if (isset($_GET['id'])) {
-    $id = isset($_GET['id']);
+    $id = $_GET['id'];
     $sql_code = "DELETE FROM `partituras` WHERE id = '$id'";
     ($sql_query = $mysqli->query($sql_code)) or
         die('Falha na execução do código: ' . $mysqli->error);
